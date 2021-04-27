2e8d29f14839a9f426f4d949bd9830d4d1ac7d3a
Rasoul Mesghali <rmesghali@acceptto.com>
2021-04-27 18:26:50 +0300

    Modified app.py for fixing postman error

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask
 from flask_restful import Api
@@ -12,7 +13,10 @@ from resources.store import Store, StoreList
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgres://ojhrjinhjhumlt:8c71e921f620fa35641c64e882814c2ae88d5266e9d9cb8e07dd2f285dfee4ca@ec2-34-240-75-196.eu-west-1.compute.amazonaws.com:5432/dfgvgi5be0ji7s', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = app.config['SQLALCHEMY_DATABASE_URI']
+if uri.startswith("postgres://"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = uri.replace("postgres://", "postgresql://", 1)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
